28ee33d528b0cfe1ad7ca1ff0cf9318e3977702e
billyovo <30436689+billyovo@users.noreply.github.com>
2022-11-02 20:56:37 +0800

    test deploy

--- .storybook/main.js
+++ .storybook/main.js
@@ -16,11 +16,15 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
-  viteFinal: (config, { configType }) => {
-    if (configType === 'PRODUCTION') {
-      config.base = './';
-    }
-
-    return config;
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    return mergeConfig(config, {
+      // Use the same "resolve" configuration as your app
+      resolve: (await import('../vite.config.js')).default.resolve,
+      // Add dependencies to pre-optimization
+      optimizeDeps: {
+        include: ['storybook-dark-mode'],
+      },
+    });
   },
 }
